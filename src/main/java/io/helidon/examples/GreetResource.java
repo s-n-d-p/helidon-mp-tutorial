ccfe72ac40bad758eeb8f3dd79542c90b710802d
sandeep_z_gupta <sandeep.z.gupta@oracle.com>
2020-05-16 20:26:43 +0530

    extract url part, PUT request

--- src/main/java/io/helidon/examples/GreetResource.java
+++ src/main/java/io/helidon/examples/GreetResource.java
@@ -7,10 +7,14 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/greet") // set the path for this resource
 @RequestScoped // define this bean as RequestScoped => request scope is active only for the
@@ -30,6 +34,28 @@ public class GreetResource {
     @Produces(MediaType.APPLICATION_JSON) // this method will return JSON data
     public JsonObject getDefaultMessage() {
         return createResponse("World");
+    }
+
+    @Path("/{name}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getMessage(@PathParam("name") String name) {
+        return createResponse(name);
+    }
+
+    @Path("/greeting")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateGreeting(JsonObject jsonObject) {
+        if (!jsonObject.containsKey("greeting")) {
+            JsonObject entity = JSON.createObjectBuilder().add("error", "No greeting provided").build();
+            return Response.status(Response.Status.BAD_REQUEST).entity(entity).build();
+        }
+
+        String newGreeting = jsonObject.getString("greeting");
+        greetingProvider.setMessage(newGreeting);
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     private JsonObject createResponse(String who) {
