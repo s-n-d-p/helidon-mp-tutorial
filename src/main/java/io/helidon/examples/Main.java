09e9b50a5e82757ace62b8531a10112814b6200f
sandeep_z_gupta <sandeep.z.gupta@oracle.com>
2020-05-16 20:38:39 +0530

    improve logging

--- src/main/java/io/helidon/examples/Main.java
+++ src/main/java/io/helidon/examples/Main.java
@@ -1,5 +1,8 @@
 package io.helidon.examples;
 
+import java.io.IOException;
+import java.util.logging.LogManager;
+
 import io.helidon.microprofile.server.Server;
 
 public final class Main {
@@ -7,9 +10,14 @@ public final class Main {
     private Main() {
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException {
+        setUpLogging();
         final Server server = startServer();
         System.out.println("http://localhost:" + server.port() + "/greet");
+    }
+
+    private static void setUpLogging() throws IOException {
+        LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("/logging.properties"));
     }
 
     static Server startServer() {
